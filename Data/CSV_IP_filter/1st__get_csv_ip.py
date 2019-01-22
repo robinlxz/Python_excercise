34577efc9a202ef086abded87bff646045614ff5
Xuzheng <linxz@garena.com>
2019-01-23 06:48:30 +0800

    call API with group IP

--- Data/CSV_IP_filter/1st__get_csv_ip.py
+++ Data/CSV_IP_filter/1st__get_csv_ip.py
@@ -40,65 +40,36 @@ def get_geo_multiple_ip(list_ip):
 	string_ip = ','.join(list_ip)
 	#print string_ip
 	url = prefix + string_ip
-	#print 'url is', url
 	r = requests.get(url).text
-	#print 'respond is', r
 	try:
 		r_dict = json.loads(r)
 	except:
 		print 'Fail to convert r to r_dict, r is %s' %r
-	#print r_dict
-	return r_dict
-
+	print r_dict
 
 def filter_only_ID_IP(L_json):
 	L_id_json = []
 	for i in L_json:
 		if i['countryCode'] == 'ID':
 			L_id_json.append(i)
-			print 'This data belong to ID \n', i, '\n' 
 	print 'there are %s sets of data collected' %len(L_id_json)
 	return L_id_json
 
-def isID_IP_single(s_json):
-	if s_json['countryCode'] == 'ID':
-		return True
-	else:
-		return False
-
 def main():
-	list_all = read_csv('100_lines.csv')
+	list_all = read_csv('10_lines.csv')
 	#print list_all
 
 	# Get IP 
 	## But this step de-couple the ip with the original record, so is only for test.
 	## In final application, may record the index for which line, or use list_all[i][4] directly
 	list_ip = [i[4] for i in list_all]
-	print 'There are %d of IPs from csv.' %len(list_ip)
-	#L_json = get_geo_from_ip(list_ip)
+	L_json = get_geo_from_ip(list_ip)
 
-	#Due to multiple ip is restricted to 32 in the API for each call, separate the list
-	n = 32
-	l_ip_group = [list_ip[i:i + n] for i in xrange(0, len(list_ip), n)]
 	#print L_json
-	#print 'l_ip_group is', l_ip_group
-	print 'There are %d groups of IP, each contains %d IPs.' %(len(l_ip_group), n)
-	l_id = []
-	i = 0
-	for l_ip in l_ip_group:
-		#print 'l_ip is:', l_ip
-		l_dict = get_geo_multiple_ip(l_ip) #this failed
-		#print 'l_dict is:', l_dict
-		l_id.append(l_dict) 
-		#print 'l_id is:', l_id
-		i += 1
-		#print 'i=',i
-	#print l_id
-	for i in l_id[0]:
-		#print 'l_id[0][i] is: ', l_id[0][i]
-		#print 'type of l_id[0][i] is:', type(l_id[0][i])
-		if isID_IP_single(l_id[0][i]):
-			print 'This Data belong to ID \n', l_id[0][i], '\n' 
+	L_id = filter_only_ID_IP(L_json)
+	print L_id
+
+
 
 if __name__ == '__main__':
 	main()
