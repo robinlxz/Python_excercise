6d11897eee20f72091412e417ee9b3159e99ea17
Xuzheng <linxz@garena.com>
2019-02-14 10:23:02 +0800

    restructure the code, remove some repeated parts

--- Data/CSV_log_filter/test.py
+++ Data/CSV_log_filter/test.py
@@ -4,6 +4,7 @@ import csv
 #import requests
 import json
 
+FILEPATH='/Users/linxz/Documents/20190125_server_notification_log/'
 
 def read_csv(file_name):
     ''' This function read the data by raws in csv, into a list '''
@@ -22,110 +23,51 @@ def read_csv(file_name):
 #     print " 'timestamp': 1546790438" in list_all[0]
 
 def getListFromSourceCSV():
-    list_071_0107 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/100071_01_07.csv')
-    list_071_0108 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/100071_01_08.csv')
-    list_072_0107 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/100072_01_07.csv')
-    list_072_0108 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/100072_01_08.csv')
-    list_836_0107 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/32836_01_07.csv')
-    list_836_0108 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/32836_01_08.csv')
-    list_837_0107 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/32837_01_07.csv')
-    list_837_0108 = read_csv('/Users/linxz/Documents/20190125_server_notification_log/32837_01_08.csv')
-#    print len(list_071_0107)
-#    print len(list_071_0108)
+    list_071_0107 = read_csv(FILEPATH+'100071_01_07.csv')
+    list_071_0108 = read_csv(FILEPATH+'100071_01_08.csv')
+    list_072_0107 = read_csv(FILEPATH+'100072_01_07.csv')
+    list_072_0108 = read_csv(FILEPATH+'100072_01_08.csv')
+    list_836_0107 = read_csv(FILEPATH+'32836_01_07.csv')
+    list_836_0108 = read_csv(FILEPATH+'32836_01_08.csv')
+    list_837_0107 = read_csv(FILEPATH+'32837_01_07.csv')
+    list_837_0108 = read_csv(FILEPATH+'32837_01_08.csv')
+    #print len(list_071_0107)
+    #print len(list_071_0108)
     fo4mth = []
     fo4mvn = []
     fo4th = []
     fo4vn = []
-    for x in list_071_0107:
-        if 'timestamp' in x[7]:
-            fo4mth.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4mth.append(x[6][14:])
-        try:
-            int(fo4mth[-1])
-        except:
-            print 'fo4mth[-1] as %s doesn\'t fit' %fo4mth[-1]
+    def getTimestampFromList(res, input_list):
+        for x in input_list:
+            if 'timestamp' in x[7]:
+                res.append(x[7][14:])
+            elif 'timestamp' in x[6]:
+                res.append(x[6][14:])
+            try:
+                int(res[-1])
+            except:
+                print 'res[-1] as %s doesn\'t fit' % res[-1]
+                return False
+        return True
 
-    for x in list_071_0108:
-        if 'timestamp' in x[7]:
-            fo4mth.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4mth.append(x[6][14:])
-        try:
-            int(fo4mth[-1])
-        except:
-            print 'fo4mth[-1] as %s doesn\'t fit' %fo4mth[-1]
+    if getTimestampFromList(fo4mth, list_071_0107) and getTimestampFromList(fo4mth, list_071_0108):
+        print 'fo4mth timestamp collected, in total %s lines' %len(fo4mth)
+    if getTimestampFromList(fo4mvn, list_072_0107) and getTimestampFromList(fo4mvn, list_072_0108):
+        print 'fo4mvn timestamp collected, in total %s lines' %len(fo4mvn)
+    if getTimestampFromList(fo4th, list_836_0107) and getTimestampFromList(fo4th, list_836_0108):
+        print 'fo4th timestamp collected, in total %s lines' %len(fo4th)
+    if getTimestampFromList(fo4vn, list_837_0107) and getTimestampFromList(fo4vn, list_837_0108):
+        print 'fo4vn timestamp collected, in total %s lines' %len(fo4vn)
 
-    for x in list_072_0107:
-        if 'timestamp' in x[7]:
-            fo4mvn.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4mvn.append(x[6][14:])
-        try:
-            int(fo4mvn[-1])
-        except:
-            print 'fo4mvn[-1] as %s doesn\'t fit' %fo4mvn[-1]
-
-    for x in list_072_0108:
-        if 'timestamp' in x[7]:
-            fo4mvn.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4mvn.append(x[6][14:])
-        try:
-            int(fo4mvn[-1])
-        except:
-            print 'fo4mvn[-1] as %s doesn\'t fit' %fo4mvn[-1]
-
-#below is to read fo4 data record
-    for x in list_836_0107:
-        if 'timestamp' in x[7]:
-            fo4th.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4th.append(x[6][14:])
-        try:
-            int(fo4th[-1])
-        except:
-            print 'fo4th[-1] as %s doesn\'t fit' %fo4th[-1]
-
-    for x in list_836_0108:
-        if 'timestamp' in x[7]:
-            fo4th.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4th.append(x[6][14:])
-        try:
-            int(fo4th[-1])
-        except:
-            print 'fo4th[-1] as %s doesn\'t fit' %fo4th[-1]
-
-    for x in list_837_0107:
-        if 'timestamp' in x[7]:
-            fo4vn.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4vn.append(x[6][14:])
-        try:
-            int(fo4vn[-1])
-        except:
-            print 'fo4vn[-1] as %s doesn\'t fit' %fo4vn[-1]
-
-    for x in list_837_0108:
-        if 'timestamp' in x[7]:
-            fo4vn.append(x[7][14:])
-        elif 'timestamp' in x[6]:
-            fo4vn.append(x[6][14:])
-        try:
-            int(fo4vn[-1])
-        except:
-            print 'fo4vn[-1] as %s doesn\'t fit' %fo4vn[-1]
-
-    #print len(fo4mth)
-    #print len(fo4mvn)
-    #print len(fo4th)
-    #print len(fo4vn)
+    # print len(fo4mth)
+    # print len(fo4mvn)
+    # print len(fo4th)
+    # print len(fo4vn)
     return [fo4mth,fo4th,fo4mvn,fo4vn]
 
 def main():
-    list_th_record = read_csv('/Users/linxz/Documents/20190125_server_notification_log/gop_log_th.csv')
-    list_vn_record = read_csv('/Users/linxz/Documents/20190125_server_notification_log/gop_log_vn.csv')
+    list_th_record = read_csv(FILEPATH+'gop_log_th.csv')
+    list_vn_record = read_csv(FILEPATH+'gop_log_vn.csv')
     #print len(list_th_record)
     #print len(list_vn_record)
     [fo4mth, fo4th, fo4mvn, fo4vn] = getListFromSourceCSV()
