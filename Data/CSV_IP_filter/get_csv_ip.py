d367d5b311d63343fbf62aceaa76bd75935ab4e0
Xuzheng <linxz@garena.com>
2019-02-15 23:23:31 +0800

    tuple as key works

--- Data/CSV_IP_filter/get_csv_ip.py
+++ Data/CSV_IP_filter/get_csv_ip.py
@@ -6,150 +6,158 @@ import json
 from time import sleep
 import pygeoip
 
-#Ugly Global varialbe for easily change code
-country_name = 'MY'
+# Ugly Global varialbe for easily change code
+COUNTRY_NAME = 'MY'
+
 
 def read_csv(file_name):
-	''' This function read the data by raws in csv, into a list '''
-	with open(file_name, 'rb') as f:
-		reader = csv.reader(f)
-		list_output = list(reader)
-	# To get rid of the first raw, if it is the title from mongo export
-	if list_output[0][0] == '_id':
-		list_output = list_output[1:]
+    ''' This function read the data by raws in csv, into a list '''
+    with open(file_name, 'rb') as f:
+        reader = csv.reader(f)
+        list_output = list(reader)
+    # To get rid of the first raw, if it is the title from mongo export
+    if list_output[0][0] == '_id':
+        list_output = list_output[1:]
 
-	return list_output
-
-#This is get ip by a free url API, has limits on calling times
-def get_geo_from_ip(list_ip): #For online API method only
-	''' Use the API to get geo info for each IP, store them into list
-	Input: a list of IP
-	Return: a list contains all dict element return by API for each IP'''
-	L = []
-	prefix = 'http://api.db-ip.com/v2/free/'
-	for ip in list_ip:
-	#	ip = '184.22.218.93'
-		url = prefix + str(ip)
-		print url
-		r = requests.get(url).text
-		try:
-			r_dict = json.loads(r)
-		except:
-			print 'Fail to convert r to r_dict, r is %s' %r
-		L.append(r_dict)
-	return L
-
-#This is get ip by a free url API, has limits on calling times
-def get_geo_multiple_ip(list_ip): #For online API method only
-	'''To call API only once, with all the IP in the list argument'''
-	prefix = 'http://api.db-ip.com/v2/free/'
-	string_ip = ','.join(list_ip)
-	#print string_ip
-	url = prefix + string_ip
-	#print 'url is', url
-	r = requests.get(url).text
-	#print 'respond is', r
-	try:
-		r_dict = json.loads(r)
-	except:
-		print 'Fail to convert r to r_dict, r is %s' %r
-	#print r_dict
-	return r_dict
+    return list_output
 
 
-def filter_only_ID_IP(L_json): #For online API method only
-	L_id_json = []
-	for i in L_json:
-		if i['countryCode'] == country_name:
-			L_id_json.append(i)
-			print 'This data belong to ID \n', i, '\n' 
-	print 'there are %s sets of data collected' %len(L_id_json)
-	return L_id_json
+# This is get ip by a free url API, has limits on calling times
+def get_geo_from_ip(list_ip):  # For online API method only
+    ''' Use the API to get geo info for each IP, store them into list
+    Input: a list of IP
+    Return: a list contains all dict element return by API for each IP'''
+    L = []
+    prefix = 'http://api.db-ip.com/v2/free/'
+    for ip in list_ip:
+        #	ip = '184.22.218.93'
+        url = prefix + str(ip)
+        print url
+        r = requests.get(url).text
+        try:
+            r_dict = json.loads(r)
+        except:
+            print 'Fail to convert r to r_dict, r is %s' % r
+        L.append(r_dict)
+    return L
+
+
+# This is get ip by a free url API, has limits on calling times
+def get_geo_multiple_ip(list_ip):  # For online API method only
+    '''To call API only once, with all the IP in the list argument'''
+    prefix = 'http://api.db-ip.com/v2/free/'
+    string_ip = ','.join(list_ip)
+    # print string_ip
+    url = prefix + string_ip
+    # print 'url is', url
+    r = requests.get(url).text
+    # print 'respond is', r
+    try:
+        r_dict = json.loads(r)
+    except:
+        print 'Fail to convert r to r_dict, r is %s' % r
+    # print r_dict
+    return r_dict
+
+
+def filter_only_ID_IP(L_json):  # For online API method only
+    L_id_json = []
+    for i in L_json:
+        if i['countryCode'] == COUNTRY_NAME:
+            L_id_json.append(i)
+            print 'This data belong to ID \n', i, '\n'
+    print 'there are %s sets of data collected' % len(L_id_json)
+    return L_id_json
+
 
 def stop_here():
-	raw_input('exiting now...') #for debug
-	exit() #for debug
+    raw_input('exiting now...')  # for debug
+    exit()  # for debug
 
-def find_ID_IP_by_API(l_ip_group): #This part of code is only copy from main, cannot use
-	l_id = []
-	i = 0
-	for l_ip in l_ip_group:
-		#print 'l_ip is:', l_ip
-		l_dict = get_geo_multiple_ip(l_ip)
 
-		#print 'l_dict is:', l_dict
-		## l_id is a list of dicts, each dicts contains 32 key pairs
-		## each pair is{IP: {return from API}}
-		l_id.append(l_dict) 		
-		#print 'i for l_id(i) now =',i
-		#print 'length for l_id now =',len(l_id)
-	#print l_id
-		for p in l_id[i]:
-			#print 'l_id[0][i] is: ', l_id[0][i]
-			#print 'type of l_id[0][i] is:', type(l_id[0][i])
-			if isID_IP_single(l_id[i][p]):
-				print 'This Data from ip ##', p, '##belong to ID \n', l_id[i][p], '\n'
-		i += 1
-		sleep(1) #To prevent API response: OVER_QUERY_LIMIT
-	return l_id	
+def find_ID_IP_by_API(l_ip_group):  # This part of code is only copy from main, cannot use
+    l_id = []
+    i = 0
+    for l_ip in l_ip_group:
+        # print 'l_ip is:', l_ip
+        l_dict = get_geo_multiple_ip(l_ip)
 
-def isID_IP_single(s_json): #For online API method only
-	try:
-#		if s_json['countryCode'] == 'ID':
-		if s_json['countryCode'] == country_name:
-			return True
-		else:
-			return False
-	except:
-		print 'now type of s_json is:', type(s_json)
-		print 's_json is:', s_json
-		stop_here()
-		return False
+        # print 'l_dict is:', l_dict
+        ## l_id is a list of dicts, each dicts contains 32 key pairs
+        ## each pair is{IP: {return from API}}
+        l_id.append(l_dict)
+        # print 'i for l_id(i) now =',i
+        # print 'length for l_id now =',len(l_id)
+        # print l_id
+        for p in l_id[i]:
+            # print 'l_id[0][i] is: ', l_id[0][i]
+            # print 'type of l_id[0][i] is:', type(l_id[0][i])
+            if isID_IP_single(l_id[i][p]):
+                print 'This Data from ip ##', p, '##belong to ID \n', l_id[i][p], '\n'
+        i += 1
+        sleep(1)  # To prevent API response: OVER_QUERY_LIMIT
+    return l_id
 
-def write_to_file(list_to_write,file_name):
-	'''This function write a list to a csv file'''
-	with open(file_name, 'w') as f:
-	    for item in list_to_write:
-	        print >> f, item
+
+def isID_IP_single(s_json):  # For online API method only
+    try:
+        #		if s_json['countryCode'] == 'ID':
+        if s_json['countryCode'] == COUNTRY_NAME:
+            return True
+        else:
+            return False
+    except:
+        print 'now type of s_json is:', type(s_json)
+        print 's_json is:', s_json
+        stop_here()
+        return False
+
+
+def write_to_file(list_to_write, file_name):
+    '''This function write a list to a csv file'''
+    with open(file_name, 'w') as f:
+        for item in list_to_write:
+            print >> f, item
+
 
 def main():
-	print 'country_name is:', country_name
-	#list_all = read_csv('10000_lines.csv')
-	list_all = read_csv('/Users/linxz/Documents/IP_filter_FO4TH/FO4_MATCH_IP.csv')
+    print 'country_name is:', COUNTRY_NAME
+    # list_all = read_csv('10000_lines.csv')
+    list_all = read_csv('/Users/linxz/Documents/IP_filter_FO4TH/FO4_MATCH_IP.csv')
 
-	# Get IP 
-	## But this step de-couple the ip with the original record, so is only for test.
-	## In final application, may record the index for which line, or use list_all[i][4] directly
-	list_ip = [i[4] for i in list_all]
-	print 'There are %d of records from csv.' %len(list_ip)
-	set_ip = set(list_ip)
-	print 'There are %d of uniq IPs from csv.' %len(set_ip)
-	#L_json = get_geo_from_ip(list_ip)
+    # Get IP
+    ## But this step de-couple the ip with the original record, so is only for test.
+    ## In final application, may record the index for which line, or use list_all[i][4] directly
+    list_ip = [i[4] for i in list_all]
+    print 'There are %d of records from csv.' % len(list_ip)
+    set_ip = set(list_ip)
+    print 'There are %d of uniq IPs from csv.' % len(set_ip)
+    # L_json = get_geo_from_ip(list_ip)
 
-	#Use geoip to get country for each IP, append to the last of list_all
-	gi4 = pygeoip.GeoIP('/Users/linxz/Documents/GitHub/Python_excercise/Data/CSV_IP_filter/TonyIP.dat', pygeoip.MEMORY_CACHE)
-	ip_country_dict = {}	
-	#Here I put selected country IP into dict only
-	for ip in list_ip:
-		if gi4.country_code_by_addr(ip) == country_name:
-			ip_country_dict[ip]=gi4.country_code_by_addr(ip)
-	print ip_country_dict
-	
-	#Here I collect the only the IP out
-	ip_country_list = ip_country_dict.keys()
-	print ip_country_list
+    # Use geoip to get country for each IP, append to the last of list_all
+    gi4 = pygeoip.GeoIP('/Users/linxz/Documents/GitHub/Python_excercise/Data/CSV_IP_filter/TonyIP.dat',
+                        pygeoip.MEMORY_CACHE)
+    ip_country_dict = {}
+    # Here I put selected country IP into dict only
+    for ip in list_ip:
+        if gi4.country_code_by_addr(ip) == COUNTRY_NAME:
+            ip_country_dict[ip] = gi4.country_code_by_addr(ip)
+    print ip_country_dict
 
-	write_to_file(ip_country_list,'IP_out_put_record.csv')
+    # Here I collect the only the IP out
+    ip_country_list = ip_country_dict.keys()
+    print ip_country_list
 
-	#Take the original record out
-	result_list = []
-	for record in list_all:
-		if record[4] in ip_country_list:
-			result_list.append(record)
+    write_to_file(ip_country_list, 'IP_out_put_record.csv')
 
-	write_to_file(result_list,'Match_out_put_record.csv')
+    # Take the original record out
+    result_list = []
+    for record in list_all:
+        if record[4] in ip_country_list:
+            result_list.append(record)
+
+    write_to_file(result_list, 'Match_out_put_record.csv')
+
 
 if __name__ == '__main__':
-	main()
-
-
+    main()
--- Data/CSV_log_filter/get_platform_and_channel_v2.py
+++ Data/CSV_log_filter/get_platform_and_channel_v2.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
-import csv
-#import requests
-import json
+'''
+#xminds
+1. import all 10071/72,32836/87 csv
+2. take out 10071[i][7] values into a list
+3. convert to CSV and import Compare_GOP_CSLOG_20190107_20190108 to a list.
+list_original
+4.
+if " 'timestamp': "+str(list_original[i][6]) in 10071
+list_original[i].append(fo4m)
+if the pair is in 32836
+list_original[i].append(fo4)
+else:
+	print 'error, %s not exist' %list_original[i][6]
+5. export list_original
+_______________________
+1. make a list consist of tuple (uid, timestamp)
+2. from csv, check if it is in the tuple
+'''
 
 FILEPATH='/Users/linxz/Documents/20190125_server_notification_log/'
 
@@ -22,13 +37,12 @@ def getListFromSourceCSV():
     list_836_0108 = read_csv(FILEPATH+'32836_01_08.csv')
     list_837_0107 = read_csv(FILEPATH+'32837_01_07.csv')
     list_837_0108 = read_csv(FILEPATH+'32837_01_08.csv')
-    #fo4mth = []
     fo4mth = {}
-    fo4mvn = []
-    fo4th = []
-    fo4vn = []
+    fo4mvn = {}
+    fo4th = {}
+    fo4vn = {}
     def getTimestampFromList(res, input_list):
-        print input_list[0][17][-1]
+        print input_list[0][14][18:]
         # print input_list[1][16]
         exit()
         for x in input_list:
@@ -46,60 +60,69 @@ def getListFromSourceCSV():
         return True
 
     def getTimestampFromList(res_dict, input_list, label):
-        print input_list[0][17][-1]
-        print input_list[1][16]
+        print input_list[0][14][18:]
+        print input_list[1][13][18:]
+ #       exit()
         assert type(label) == str
 
-        platformDict = {'0':'ios', '1':'aos', '2':'other'}
+        #platformDict = {'0':'testios', '1':'testaos', '2':'testother', '3':'test3', '4':'test4'}
 
         for x in input_list:
             # if input_list.index(x) == 210:
             #     print ('uid:' + x[5][9:], 'ts:' + x[6][14:])
             #     res_dict[('uid:' + x[5][9:], 'ts:' + x[6][14:])] = 1
             #     exit()
-            try:
-                if 'timestamp' in x[7] and 'uid' in x[6]:
-                    print ('uid:'+x[6][9:],'ts:'+x[7][14:])
-                    print input_list.index(x)
-                    res_dict[('uid:'+x[6][9:],'ts:'+x[7][14:])] = (label,platformDict[x[17][-1]])
-                elif 'timestamp' in x[6] and 'uid' in x[5]: #Because some data have misplacement
-                    print ('uid:' + x[5][9:], 'ts:' + x[6][14:])
-                    print input_list.index(x)
-                    res_dict[(('uid:'+x[5][9:],'ts:'+x[6][14:]))] = (label,platformDict[x[16][-1]])
-                else:
-                    print 'this line of data as doesn\'t fit. \n%s\n' %x
-                    return False
-            except:
-                print 'fail here'
-                print input_list.index(x)
-                exit()
+            #try:
+            if 'timestamp' in x[7] and 'uid' in x[6]:
+                #print ('uid:'+x[6][9:],'ts:'+x[7][14:])
+                #print input_list.index(x)
+                res_dict[('uid:'+x[6][9:],'ts:'+x[7][14:])] = (label, 'ch:'+x[14][18:])
+            elif 'timestamp' in x[6] and 'uid' in x[5]: #Because some data have misplacement
+                #print ('uid:' + x[5][9:], 'ts:' + x[6][14:])
+                #print input_list.index(x)
+                res_dict[(('uid:'+x[5][9:],'ts:'+x[6][14:]))] = (label,'ch:'+x[13][18:])
+            else:
+                print 'this line of data as doesn\'t fit. \n%s\n' %x
+                return False
+        print 'now res_dict as the length', len(res_dict)
         return True
 
     if getTimestampFromList(fo4mth, list_071_0107, 'fo4mth') and getTimestampFromList(fo4mth, list_071_0108, 'fo4mth'):
         print 'fo4mth timestamp collected, in total %s lines' %len(fo4mth)
-    print 'debug till here is fine'
-    exit()
-    if getTimestampFromList(fo4mvn, list_072_0107) and getTimestampFromList(fo4mvn, list_072_0108):
+    if getTimestampFromList(fo4mvn, list_072_0107, 'fo4mvn') and getTimestampFromList(fo4mvn, list_072_0108, 'fo4mvn'):
         print 'fo4mvn timestamp collected, in total %s lines' %len(fo4mvn)
-    if getTimestampFromList(fo4th, list_836_0107) and getTimestampFromList(fo4th, list_836_0108):
+    if getTimestampFromList(fo4th, list_836_0107, 'fo4th') and getTimestampFromList(fo4th, list_836_0108, 'fo4th'):
         print 'fo4th timestamp collected, in total %s lines' %len(fo4th)
-    if getTimestampFromList(fo4vn, list_837_0107) and getTimestampFromList(fo4vn, list_837_0108):
+    if getTimestampFromList(fo4vn, list_837_0107, 'fo4vn') and getTimestampFromList(fo4vn, list_837_0108, 'fo4vn'):
         print 'fo4vn timestamp collected, in total %s lines' %len(fo4vn)
-
     return [fo4mth, fo4th, fo4mvn, fo4vn]
 
+
 def matchToAppend(list_record, match_list, append_str):
+    #This is to matchList
     for x in list_record:
         #print ('uid:'+x[0],'ts:'+x[5])
         #print match_list[1]
         if ('uid:'+x[0],'ts:'+x[5]) in match_list:
             x.append(append_str)
-        #else:
-        #    print 'x is not found in the' + append_str + 'record, timestamp is %s, it is the %s line' %(x[5], list_record.index(x))
+        else:
+            print 'x is not found in the' + append_str + 'record, timestamp is %s, it is the %s line' %(x[5], list_record.index(x))
     print len(list_record)
     return list_record
 
-
+def matchDictToAppend(list_record, match_dict, append_str):
+    '''For each item in list_record, append its value in the match_dict'''
+    assert type(list_record) == list
+    assert type(match_dict) == dict
+    for x in list_record:
+        if ('uid:'+x[0],'ts:'+x[5]) in match_dict.keys():
+            x.append(match_dict[('uid:'+x[0],'ts:'+x[5])][0])
+            print match_dict[('uid:' + x[0], 'ts:' + x[5])][1][3:]
+            x.append(match_dict[('uid:' + x[0], 'ts:' + x[5])][1][3:])
+        # else:
+        #     print 'x is not found in the' + append_str + 'record, timestamp is %s, it is the %s line' %(x[5], list_record.index(x))
+    print len(list_record)
+    return list_record
 
 def main():
     list_th_record = read_csv(FILEPATH+'gop_log_th.csv')
@@ -107,10 +130,10 @@ def main():
 
     [fo4mth, fo4th, fo4mvn, fo4vn] = getListFromSourceCSV()
 
-    list_th_record = matchToAppend(list_th_record, fo4mth, 'fo4mth')
-    list_th_record = matchToAppend(list_th_record, fo4th, 'fo4th')
-    list_vn_record = matchToAppend(list_vn_record, fo4mvn, 'fo4mvn')
-    list_vn_record = matchToAppend(list_vn_record, fo4vn, 'fo4vn')
+    list_th_record = matchDictToAppend(list_th_record, fo4mth, 'fo4mth')
+    list_th_record = matchDictToAppend(list_th_record, fo4th, 'fo4th')
+    list_vn_record = matchDictToAppend(list_vn_record, fo4mvn, 'fo4mvn')
+    list_vn_record = matchDictToAppend(list_vn_record, fo4vn, 'fo4vn')
     # for x in list_th_record:
     #     print ('uid:'+x[0],'ts:'+x[5])
     #     if ('uid:'+x[0],'ts:'+x[5]) in fo4mth:
