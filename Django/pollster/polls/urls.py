37f056aebb382ae8ed386d649b3a94927a3caf0c
Xuzheng <linxz@garena.com>
2020-01-09 21:00:13 +0800

    UPD: fix pollster typo for url name

--- Django/pollster/polls/urls.py
+++ Django/pollster/polls/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # path('<int:question_id>/detail/', views.detail, name='detail'),
     #'''
     # path('<int:question_id>/results/', views.results, name='results'), 
-    path('<int:question_id>/resultssss/', views.results, name='result'),
+    path('<int:question_id>/resultssss/', views.results, name='results'),
     # What if the name='results' changed? where else should I change to make the link work again? Made: it's defined in index.html href, it go here based on the href="{% url 'polls:result' question.id %}"
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Django/pollster/polls/views.py
+++ Django/pollster/polls/views.py
@@ -45,5 +45,5 @@ def vote(request, question_id):
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+        # user hits the Back button.        
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
